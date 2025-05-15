21e2a4cf14eb7f2646dad1877b5411edbfb179ff
PopovDanil <154554866+PopovDanil@users.noreply.github.com>
2025-05-15 15:17:25 +0300

    Add files via upload

---
+++ cube.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math"
+)
+
+var (
+	output 			[]rune
+	zBuffer 		[]float64
+	screenHeight 	int = 70
+	screenWidght 	int = 120
+	cubeWidght 		float64 = 80.0
+	k2 				float64 = 250.0
+	k1 				float64 = 40.0
+	A 				float64 = 0.0
+	B 				float64 = 0.0
+	C				float64 = 0.0
+	incrementSpeed  float64 = 1.8
+)
+
+func clear() {
+	for i := 0; i < screenHeight * screenWidght; i++ {
+		output[i] = ' '
+		zBuffer[i] = 0.0
+	}
+}
+
+func render(x, y, z float64, symb rune) {
+
+	cosA := math.Cos(A)
+	sinA := math.Sin(A)
+	cosB := math.Cos(B)
+	sinB := math.Sin(B)
+	cosC := math.Cos(C)
+	sinC := math.Sin(C)
+
+	xRot := cosA * cosB * x + (cosA * sinB * sinC - sinA * cosC) * y + (cosA * sinB * cosC + sinA * sinC) * z
+	yRot := sinA * cosB * x + (sinA * sinB * sinC + cosA * cosC) * y + (sinA * sinB * cosC - cosA * sinC) * z
+	zRot := k2 - sinB * x + cosB * sinC * y + cosB * cosC * z
+	ooz := 1.0 / zRot
+
+	xp := int(float64(screenWidght) / 2 + ooz * k1 * xRot)
+	yp := int(float64(screenHeight) / 2 + ooz * k1 * 0.5 * yRot)
+	idx := yp * screenWidght + xp
+
+	if idx >= 0 && idx < screenHeight * screenWidght {
+		if ooz > zBuffer[idx] {
+			zBuffer[idx] = ooz
+			output[idx] = symb
+		}
+	}
+
+}
+
+func print() {
+	fmt.Print("\x1b[H")
+	for i := 0; i < screenHeight; i++ {
+		fmt.Println(string(output[i * screenWidght : (i + 1) * screenWidght]))
+	}
+}
+
+func main() {
+	output = make([]rune, screenHeight * screenWidght)
+	zBuffer = make([]float64, screenHeight * screenWidght)
+
+	ticker := time.NewTicker(16 * time.Millisecond)
+	defer ticker.Stop()
+
+	for range ticker.C {
+
+		clear()
+
+		for x := -cubeWidght; x < cubeWidght; x += incrementSpeed {
+			for y := -cubeWidght; y < cubeWidght; y += incrementSpeed {
+				render(x, y, -cubeWidght, '#')
+				render(cubeWidght, y, x, ')')
+				render(-cubeWidght, y, -x, '&')
+				render(-x, y, cubeWidght, '+')
+				render(x, -cubeWidght, -y, '=')
+				render(x, cubeWidght, y, '(')
+			}
+		}
+
+		print()
+
+		A += 0.014
+		B += 0.012
+		C += 0.011
+	}
+}
---
+++ torus.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math"
+)
+
+var (
+	output 			[]rune
+	zBuffer 		[]float64
+	screenHeight 	int = 70
+	screenWidght 	int = 100
+	thetaIncrement  float64 = 0.003
+	phiIncrement 	float64 = 0.007
+	r1 				float64 = 1.0
+	r2 				float64 = 2.0
+	k2 				float64 = 10.0
+	k1 				float64 = float64(screenWidght) * k2 * 3 / (8 * (r1 + r2))
+	symbols 		string = ".,-~:;=!*#$@"
+)
+
+func clear() {
+	for i := 0; i < screenHeight * screenWidght; i++ {
+		output[i] = ' '
+		zBuffer[i] = 0.0
+	}
+}
+
+func render(A, B float64) {
+
+	clear()
+
+	cosA := math.Cos(A)
+	sinA := math.Sin(A)
+	cosB := math.Cos(B)
+	sinB := math.Sin(B)
+
+	for theta := 0.0; theta < math.Pi * 2; theta += thetaIncrement {
+		cosT := math.Cos(theta)
+		sinT := math.Sin(theta)
+
+		for phi := 0.0; phi < math.Pi * 2; phi += phiIncrement {
+			cosP := math.Cos(phi)
+			sinP := math.Sin(phi)
+
+			circleX := r2 + r1 * cosT
+			circleY := r1 * sinT
+
+			x := circleX * (cosB * cosP + sinA * sinB * sinP) - circleY * cosA * sinB
+			y := circleX * (sinB * cosP - sinA * cosB * sinP) + circleY * cosA * cosB
+			z := k2 + cosA * circleX * sinP + circleY * sinA
+			ooz := 1.0 / z 
+
+			xp := int(float64(screenWidght) / 2 + ooz * k1 * x)
+			yp := int(float64(screenHeight) / 2 - ooz * k1 * 0.5 * y)
+
+			l := cosP * cosT * sinB - cosA * cosT * sinP - sinA * sinT + cosB * (cosA * sinT - cosT * sinA * sinP)
+
+			idx := yp * screenWidght + xp
+
+			if (l > 0 && idx >= 0 && idx < screenHeight * screenWidght) {
+				if (ooz > zBuffer[idx]) {
+					zBuffer[idx] = ooz
+					lumenIdx := int(l * 8.0)
+					output[idx] = rune(symbols[lumenIdx])
+				}
+			}
+		}
+	}
+
+}
+
+func print() {
+	fmt.Print("\x1b[H")
+	for i := 0; i < screenHeight; i++ {
+		fmt.Println(string(output[i * screenWidght : (i + 1) * screenWidght]))
+	}
+}
+
+func main() {
+	output = make([]rune, screenHeight * screenWidght)
+	zBuffer = make([]float64, screenHeight * screenWidght)
+
+	ticker := time.NewTicker(16 * time.Millisecond)
+	defer ticker.Stop()
+
+	A := 0.0
+	B := 0.0
+
+	for range ticker.C {
+
+		render(A, B)
+
+		print()
+
+		A += 0.04
+		B += 0.02
+	}
+}
